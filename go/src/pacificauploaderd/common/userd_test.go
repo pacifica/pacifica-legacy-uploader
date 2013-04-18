3332d68a9092612e5570d29331cc65a7c4e0a200
Craig Allwardt <craig.allwardt@pnnl.gov>
2013-04-18 12:37:15 -0700

    Added a failing test for userd.  I need to figure out a way to hook into the rpc to make this easier.

--- go/src/pacificauploaderd/common/userd_test.go
+++ go/src/pacificauploaderd/common/userd_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
 	"testing"
 )
 
@@ -11,10 +12,26 @@ func Test_FakeUserCannotAccessFile(t *testing.T) {
 	fmt.Printf("FakeUserCannotAccessfile")
 	fakeUser := "PNL\a1sdfata"
 	file, _ := exec.LookPath(os.Args[0]) // [1]
-	println("file:", file)
-	cwd, err := os.Getwd(file)
+	fmt.Println("file:", file)
+	cwd, _ := os.Getwd()
 	if _, err := UserAccess(fakeUser, cwd); err == nil {
 		t.Error("Fake user: %v was allowed access to %v", fakeUser, cwd)
 	}
-	t.Error("Just making sure it is here!")
+}
+
+func Test_CurrentUserCanAccessFile(t *testing.T) {
+	fmt.Printf("CurrentUserCanAccessFile")
+
+	u, err := user.Current()
+	if err != nil {
+		t.Error("Couldn't get current user\n%v", err)
+		return
+	}
+	file, _ := exec.LookPath(os.Args[0]) // [1]
+	fmt.Println("file:", file)
+	cwd, _ := os.Getwd()
+	if _, err := UserAccess(u.Username, cwd); err != nil {
+		t.Error("User: %v couldn't access %v\nError: %v", u.Username, cwd, err)
+		return
+	}
 }
