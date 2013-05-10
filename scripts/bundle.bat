a0b2d907b76bba7ee463f5cc0a3e9cd2d429e0b6
Craig Allwardt <craig.allwardt@pnnl.gov>
2013-05-10 13:14:38 -0700

    Updated wxs and bundle.bat files to use the release directory.

--- scripts/bundle.bat
+++ scripts/bundle.bat
@@ -1,9 +1,9 @@
 set PATH=%PATH%;c:\wix
 
 heat dir "go\src\pacificauploaderd\ui" -cg PacificaUploaderUI -gg -sfrag -dr INSTALLDIR -var var.DaemonUI -out pacificauploaderuigen.wxs || goto :error
-heat dir "build" -cg PacificaUploaderGroup -gg -sfrag -dr INSTALLDIR -var var.PacificaUploaderGroup -out pacificauploadergen.wxs
+heat dir "build\release" -srd -cg PacificaUploaderGroup -gg -sfrag -dr INSTALLDIR -var var.PacificaUploaderGroup -out pacificauploadergen.wxs
 
-candle pacificauploader.wxs pacificauploadergen.wxs pacificauploaderuigen.wxs -dDaemonUI=go\src\pacificauploaderd\ui -dPacificaUploaderGroup=build -ext WixUtilExtension || goto :error
+candle pacificauploader.wxs pacificauploadergen.wxs pacificauploaderuigen.wxs -dDaemonUI=go\src\pacificauploaderd\ui -dPacificaUploaderGroup=build\release -ext WixUtilExtension || goto :error
 light -out pacificauploader.msi pacificauploader.wixobj pacificauploadergen.wixobj pacificauploaderuigen.wixobj -ext WixUtilExtension || goto :error
 
 candle pacificauploaderui.wxs || goto :error
