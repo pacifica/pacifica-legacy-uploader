b768aa656bd9371220bd956c91260d0e731a3701
Craig Allwardt <craig.allwardt@pnnl.gov>
2013-05-13 14:16:42 -0700

    Fixed script to put the uploader service out of the "generated heat file" and then copy it back after heat has been run.

--- scripts/bundle.bat
+++ scripts/bundle.bat
@@ -1,7 +1,13 @@
 set PATH=%PATH%;c:\wix
 
+rem Copy pacificauploaderservice.exe out of the release dir so that heat doesn't include
+mv "build\release\pacificauploaderservice.exe" "build"
+
 heat dir "go\src\pacificauploaderd\ui" -cg PacificaUploaderUI -gg -sfrag -dr INSTALLDIR -var var.DaemonUI -out pacificauploaderuigen.wxs || goto :error
 heat dir "build\release" -srd -cg PacificaUploaderGroup -gg -sfrag -dr INSTALLDIR -var var.PacificaUploaderGroup -out pacificauploadergen.wxs
+
+rem Move pacificauploaderservice.exe back in the release directory.
+mv "build\pacificauploaderservice.exe" "build\release"
 
 candle pacificauploader.wxs pacificauploadergen.wxs pacificauploaderuigen.wxs -dDaemonUI=go\src\pacificauploaderd\ui -dPacificaUploaderGroup=build\release -ext WixUtilExtension || goto :error
 light -out pacificauploader.msi pacificauploader.wixobj pacificauploadergen.wixobj pacificauploaderuigen.wixobj -ext WixUtilExtension || goto :error
